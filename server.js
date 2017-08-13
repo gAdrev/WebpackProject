3a545805ade48e97fb93b4af1553d5f51ed24a80
Javier V <javiervilla00@gmail.com>
2017-08-13 20:09:54 +0200

    Serving statics directly in production

--- server.js
+++ server.js
@@ -1,13 +1,24 @@
+const path = require("path");
 const express = require("express");
-const webpackMiddleware = require("webpack-dev-middleware");
-const webpack = require("webpack");
-const webpackConfig = require("./webpack.config.js");
 
-const PORT = 3050;
+const PORT = process.env.PORT || 3050;
 
 const app = express();
 
-app.use(webpackMiddleware(webpack(webpackConfig)));
+const isDevelopment = process.env.NODE_ENV !== "production";
+if (isDevelopment) {
+  const webpackMiddleware = require("webpack-dev-middleware");
+  const webpack = require("webpack");
+  const webpackConfig = require("./webpack.config.js");
+  app.use(webpackMiddleware(webpack(webpackConfig)));
+} else {
+  app.use(express.static("dist"));
+
+  // react-router browser history module compatibility stuff
+  app.get("*", (req, res) => {
+    res.sendFile(pat.join(__dirname, "dist/index.html"));
+  })
+}
 
 app.listen(PORT, () => {
   console.log("Listening on port " + PORT);
